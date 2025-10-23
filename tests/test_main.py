83f42988ef36747b7e427bf357f72b5c541018d7
Nurdin Mohamedi <nurdinmohamedi99@gmail.com>
2025-10-23 21:44:50 +0300

    test: Write test for /todos route

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,15 @@ def test_server_working():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, World!"}
+
+
+def test_get_all_todos():
+    response = client.get("/todos")
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "title": "Create a CRUD application",
+            "description": "Create a FastAPI CRUD application for managing todos",
+            "is_complete": False,
+        }
+    ]
