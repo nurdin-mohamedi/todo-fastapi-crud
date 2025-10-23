1b0ac3c57500d71a5bba23d54504d4952bf047a1
Nurdin Mohamedi <nurdinmohamedi99@gmail.com>
2025-10-23 21:44:19 +0300

    feat: Add /todos route

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from typing import Any
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+todos = [
+    {
+        "title": "Create a CRUD application",
+        "description": "Create a FastAPI CRUD application for managing todos",
+        "is_complete": False,
+    }
+]
+
+
 @app.get("/")
 def get_home() -> dict:
     return {"message": "Hello, World!"}
+
+
+@app.get("/todos")
+def get_todos() -> list[dict[str, Any]]:
+    return todos
