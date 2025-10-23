b26858888f4ab86958f4633a40ef0380bbee44d2
Nurdin Mohamedi <nurdinmohamedi99@gmail.com>
2025-10-24 00:11:41 +0300

    feat: Respond with data from the database

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from typing import Any
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from models import TodoBase
+from schema import Todo
+from utility import get_db
 
 app = FastAPI()
-
-
-todos = [
-    {
-        "title": "Create a CRUD application",
-        "description": "Create a FastAPI CRUD application for managing todos",
-        "is_complete": False,
-    }
-]
 
 
 @app.get("/")
@@ -19,6 +15,13 @@ def get_home() -> dict:
     return {"message": "Hello, World!"}
 
 
-@app.get("/todos")
-def get_todos() -> list[dict[str, Any]]:
-    return todos
+@app.get("/todos", response_model=list[TodoBase])
+def get_todos(
+    db: Session = Depends(get_db),
+) -> Any:
+    todos: list[Todo] = db.query(Todo).all()
+    if todos:
+        print(todos[0].title)
+        print(todos[0].description)
+        return todos
+    return {"message": "Todos not found"}
