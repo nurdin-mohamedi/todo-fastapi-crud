6184c0407d284fa9a5ddcf9ee5d38d4ad509a718
Nurdin Mohamedi <nurdinmohamedi99@gmail.com>
2025-11-08 15:13:14 +0300

    test: Add tests for getting single Todo

--- tests/test_main.py
+++ tests/test_main.py
@@ -24,3 +24,26 @@ def test_get_all_todos():
             "is_complete": False,
         }
     ]
+
+
+def test_get_todos_non_found():
+    # TODO: Delete all todos
+    response = client.get("/todos")
+    assert response.status_code == 200
+    # TODO: assert response.json() == {"message": "Todo not found"}
+
+
+def test_get_todo_exist():
+    response = client.get("/todos/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        "title": "Create a CRUD application",
+        "description": "Create a FastAPI CRUD application for managing todos",
+        "is_complete": False,
+    }
+
+
+def test_get_todo_not_exist():
+    response = client.get("/todos/10")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Todo not found"}
